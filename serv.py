6a6a2a7f32613d23a259bc373d4a2a331e46be7d
Robert Jankowski <jankowski.r@hotmail.com>
2017-04-03 12:57:09 +0200

    catch exception on socket init

--- serv.py
+++ serv.py
@@ -9,15 +9,22 @@ HOST_ADDR = ''
 SOCKET_FAMILY = socket.AF_INET
 SOCKET_TYPE = socket.SOCK_STREAM
 MESSAGE_CHUNK_SIZE = 16
+server_socket = None
 #
-server_socket = socket.socket(SOCKET_FAMILY, SOCKET_TYPE)
+try:
+    server_socket = socket.socket(SOCKET_FAMILY, SOCKET_TYPE)
+except OSError as ex:
+    print(str(ex))
+    
 server_address = (HOST_ADDR, SERVER_PORT)
 try:
     server_socket.bind(server_address)
+    server_socket.listen(1)
 except socket.error as ex:
+    server_socket.close()
     print("Error: "+str(ex[0]))
     sys.exit()
-server_socket.listen(10) #max connection
+
 while True:
     client_connection_handler, client_address = server_socket.accept()
     with client_connection_handler:
